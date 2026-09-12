1ba9aced8de94ffd040fa9ff329c5d882d5ed547
Priyanshi <priyanshi067@gmail.com010>
2026-09-12 22:16:25 +0530

    Find the second largest no. in a list

---
+++ problem193.py
@@ -0,0 +1,13 @@
+numbers = [10, 25, 8, 40, 32]
+
+largest = numbers[0]
+second = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        second = largest
+        largest = num
+    elif num > second and num != largest:
+        second = num
+
+print("Second largest =", second)
