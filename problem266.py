1f2987676089c6e671a74102da98a5caa227df71
Priyanshi <priyanshi067@gmail.com010>
2026-09-17 06:58:01 +0530

    Move all zeroes to the end in an array

---
+++ problem266.py
@@ -0,0 +1,16 @@
+arr = [0, 1, 0, 3, 12]
+
+result = []
+
+zero_count = 0
+
+for num in arr:
+    if num == 0:
+        zero_count += 1
+    else:
+        result.append(num)
+
+for i in range(zero_count):
+    result.append(0)
+
+print(result)
